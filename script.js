d6e47ef732b8535b5aa34ab1499241c78d8a9654
ijdb <110166945+ijdb@users.noreply.github.com>
2022-11-16 01:29:53 +0530

    make todo list app

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-setTimeout(() => {
-  document.getElementById("jsTest").innerText = "Works";
-}, 1000);
+const inputText = document.getElementById("input-text");
+const dailyTask = document.getElementById("daily-tasks");
+const addBtn = document.getElementById("add-btn");
+function clickHandler() {
+  if (inputText.value === "") {
+    alert("Task cannot be empty");
+  } else {
+    // dailyTask.innerText += inputText.value;
+    dailyTask.innerHTML += `
+                  <div class="task">
+                  <span>${inputText.value}</span> 
+                  <i class="material-icons" onclick="handleDelete(event)">delete</i>
+                  <button class="donebtn" onclick="handleDone(event)">Done</button>
+                  </div>
+                  `;
+  }
+  inputText.value = "";
+}
+addBtn.addEventListener("click", clickHandler);
+
+function handleDone(e) {
+  const parent = e.target.closest(".task");
+  const targetSpan = parent.getElementsByTagName("span")[0];
+  targetSpan.classList.add("done");
+}
+function handleDelete(e) {
+  const parent = e.target.closest(".task");
+  parent.classList.add("deleted");
+}
